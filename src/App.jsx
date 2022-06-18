63506e5a68b80c28425e70086614bd5c8b96914e
Heytemisan <temyicy@gmail.com>
2022-06-18 11:26:55 +0100

    feat: created header and pokemonCard components

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { fetchMovies } from "./api/api";
-// require('dotenv').config();
+import React from "react";
 
 function App() {
 
-  const [movieData, setMovieData] = useState("");
-  // const getData = async () => {
-  //   const url = `https://api.themoviedb.org/3/trending/all/day?api_key={process.env.REACT_APP_SECRET_NAME}`
-
-  //   const response = await fetch(url);
-  //   try {
-  //     const responseJson = await response.json()
-  //     const data = (responseJson.results);
-  //     setMovieData(data);
-  //   } catch (error) {
-  //     console.error(error)
-  //   }
-  // }
-  useEffect(() => {
-    fetchMovies().then((movieDatas) => {
-      setMovieData(movieDatas || 'no user data');
-    });
-  }, []);
-  // console.log(fetchMovies);
-
   return (
     <>
-      {/* {movieData.map(movie=> {
-        return (
-          <div>
-            {movie.name}
-          </div>
-        )
-      })} */}
-      <section className="lists">
-        {movieData.map((item, key) => (
-          <div key={key} title={item.title} items={item.items} />
-        ))}
-      </section>
-      {movieData.total_pages}
-      <p>print env secret to HTML</p>
+      
     </>
   );
 }
---
+++ src/components/pokemonCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const pokemonCard = () => {
+    return (
+        <div>pokemonCard</div>
+    )
+}
+
+export default pokemonCard
